6c37ae66273dab12d76094ef40c322c469574415
Maksym Yaroshenko <yarrok98@gmail.com>
2022-03-21 19:56:04 +0200

    add DB connection

---
+++ QuizApi/Models/QuizDatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizApi.Models
+{
+    public class QuizDatabaseContext:DbContext
+    {
+        public QuizDatabaseContext(DbContextOptions<QuizDatabaseContext> options):base(options) 
+        { }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+    }
+}
