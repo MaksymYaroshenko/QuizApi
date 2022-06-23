a3f5265040ebffb8eb801d5a2a1c437220e5c6d8
Maksym Yaroshenko <yarrok98@gmail.com>
2022-06-23 20:54:40 +0300

    add score saving

--- QuizApi/Controllers/ParticipantController.cs
+++ QuizApi/Controllers/ParticipantController.cs
@@ -1,9 +1,4 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuizApi.Models;
@@ -45,12 +40,14 @@ namespace QuizApi.Controllers
         // PUT: api/Participant/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutParticipant(int id, Participant participant)
+        public async Task<IActionResult> PutParticipant(int id, ParticipantResult result)
         {
-            if (id != participant.ParticipantId)
-            {
+            if (id != result.ParticipantId)
                 return BadRequest();
-            }
+
+            Participant participant = await _context.Participants.FindAsync(id);
+            participant.Score = result.Score;
+            participant.TimeTaken = result.TimeTaken;
 
             _context.Entry(participant).State = EntityState.Modified;
 
